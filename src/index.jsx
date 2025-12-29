84ae5e4bebedbb029eabe98a5f3c9bb367cc00cf
Xinyue(Iris) Wang <irisita9999@gmail.com>
2025-12-28 22:06:04 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import ScrollToSectionParam from "./ScrollToSectionParam.jsx";
 import ScrollManager from "./ScrollManager.jsx";
 
 const rootEl = document.getElementById("root");
@@ -11,7 +10,6 @@ if (!rootEl) throw new Error("Root element #root not found");
 createRoot(rootEl).render(
   <React.StrictMode>
     <HashRouter>
-      <ScrollToSectionParam />
       <ScrollManager />
       <App />
     </HashRouter>
