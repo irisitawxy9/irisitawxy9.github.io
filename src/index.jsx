ad73952d6672a2549def3fa750f7228b5e6fc95f
Xinyue(Iris) Wang <irisita9999@gmail.com>
2025-12-28 22:09:15 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import ScrollManager from "./ScrollManager.jsx";
+import ScrollManager from "./ScrollManager";
 
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error("Root element #root not found");
